0fa252c3668446db780033b749c7cdc9167fe164
Samidkun <samiddasproa@gmail.com>
2025-12-07 14:33:01 +0700

    fix: cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(\App\Http\Middleware\CorsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
